516dc0f198365874ac56e3d37d55391ad051c3b1
Nathan Dao <nathan@guynathan.com>
2016-11-30 14:44:26 +0200

    complete the game - sort of

--- src/pegthing/core.clj
+++ src/pegthing/core.clj
@@ -16,17 +16,7 @@
      accumulator
      (recur (- row-size 1) (+ accumulator row-size)))))
 
-(defn make-init-board-data []
-  (reduce (fn [board-data key]
-            (assoc board-data
-                   key {:value (inc (.indexOf key-list key))
-                        :pegged true
-                        :label (name key)}))
-          {}
-          key-list))
-
 (defn get-y
-  "Get row"
   ([index] (get-y index 1))
   ([index row]
    (if (< index (get-row-first-val row))
@@ -34,9 +24,20 @@
      (recur index (+ row 1)))))
 
 (defn get-x
-  "Get col"
   [index]
   (inc (- index (get-row-first-val (get-y index)))))
+
+(defn make-init-board-data []
+  (reduce (fn [board-data key]
+            (let [index (.indexOf key-list key)]
+              (assoc board-data
+                     key {:value (inc index)
+                          :pegged true
+                          :label (name key)
+                          :x (get-x (inc index))
+                          :y (get-y (inc index))})))
+          {}
+          key-list))
 
 (defn get-coordinates
   [index]
@@ -48,8 +49,8 @@
   [cell-data]
   (string/join [(:label cell-data)
                 (condp = (:pegged cell-data)
-                  true 0
-                  false *)]))
+                  true "0"
+                  false "*")]))
 
 (defn make-board-labels
   [board-data]
@@ -99,19 +100,87 @@
   [label board-data]
   ((keyword label) board-data))
 
-(defn valid-begin?
+(defn valid-start-point?
+  "Start point should still be pegged."
   [label board-data]
   (let [point-data (get-point-data label board-data)]
     (and (not (empty? point-data))
          (:pegged point-data))))
 
-(defn valid-end?
+(def valid-middle-point? valid-start-point?)
+(def can-remove-peg? valid-start-point?)
+
+(defn valid-end-point?
+  "End point should have peg removed."
   [label board-data]
   (let [point-data (get-point-data label board-data)]
     (and (not (empty? point-data))
          (not (:pegged point-data)))))
 
-(defn valid-move?
-  [begin end board-data]
-  (and (valid-begin? begin board-data)
-       (valid-end? end board-data)))
+(defn get-point-from-cordinates
+  [point-data board-data]
+  (let [filtered (filter #(and (== (:x point-data) (:x (nth % 1)))
+                               (== (:y point-data) (:y (nth % 1))))
+                         board-data)]
+    (if (empty? filtered)
+      ""
+      (name (first (first filtered))))))
+
+(defn modify-peg
+  "Remove a peg and return the updated board data."
+  [point pegged board-data]
+  (let [point-data (get-point-data point board-data)]
+
+    (if (not-empty point-data)
+      (assoc board-data (keyword point)
+             (assoc point-data :pegged pegged))
+      board-data)))
+
+(defn remove-peg
+  [point board-data]
+  (modify-peg point false board-data))
+
+(defn add-peg
+  [point board-data]
+  (modify-peg point true board-data))
+
+(defn make-move
+  "Evaluate if the start and end point makes a valid move."
+  [start-point end-point board-data]
+  (let [start-point-data (get-point-data start-point board-data)
+        end-point-data (get-point-data end-point board-data)
+        middle-point-cordinates {:x (/ (+ (:x start-point-data)
+                                          (:x end-point-data)) 2)
+                                 :y (/ (+ (:y start-point-data)
+                                          (:y end-point-data)) 2)}
+        middle-point (get-point-from-cordinates middle-point-cordinates board-data)]
+    (if (and (valid-start-point? start-point board-data)
+             (valid-end-point? end-point board-data)
+             (and (not-empty middle-point)
+                  (valid-middle-point? middle-point board-data)))
+      (add-peg end-point
+               (remove-peg start-point
+                           (remove-peg middle-point board-data)))
+      board-data)))
+
+(defn game-loop
+  ([]
+   (print-board init-board-data)
+   (println "Choose your initial point: ")
+   (let [init-point (read-line)]
+     (game-loop init-board-data init-point)))
+
+  ([board-data]
+   (print-board board-data)
+   (println "Start: ")
+   (let [start-point (read-line)]
+     (println "End: ")
+     (let [end-point (read-line)]
+       (recur (make-move start-point end-point board-data)))))
+
+  ([board-data init-point]
+   (game-loop (remove-peg init-point board-data))))
+
+(defn -main
+  [& args]
+  (game-loop))
